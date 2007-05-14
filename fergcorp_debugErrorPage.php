52f1f99c365916c1078674d4f2b0cda8c9c453ec
fergbrain <fergbrain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-14 23:23:14 +0000

    Initial version
    git-svn-id: https://plugins.svn.wordpress.org/dunstan-error-page/branches/debug@12822 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fergcorp_debugErrorPage.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: Dunstan-style Error Page Debug
+Plugin Description: WARNING: Activating this plugin will reset all settings associated with Dunstan-style Error Page!
+Version: 0.1
+Author: Andrew Ferguson
+Author URI: http://www.fergcorp.com/
+
+Copyright (c) 2007 Andrew Ferguson. All Rights Reserved.
+
+*/
+
+add_action('activate_fergcorp_debugErrorPage.php', 'fergcorp_debugErrorPage_install');
+
+function fergcorp_debugErrorPage_install(){
+global $wpdb;
+		$wpVersion = "Version: ".get_bloginfo('version')."\n\n";
+		$optionsDump = "Options Dump:\n".$wpdb->get_var("SELECT option_value FROM $wpdb->options WHERE `option_name` = 'afdn_error_page'");
+		mail("andrew@fergcorp.com", "[Countdown Timer Debug Report]", $wpVersion.$optionsDump);
+		$wpdb->query("DELETE FROM $wpdb->options WHERE `option_name` = 'afdn_error_page'");
+
+}
+?>
